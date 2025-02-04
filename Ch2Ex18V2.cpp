15d24f11e48fa4a956fda2a1603b44f3f799d88d
Gabriel Inocentes <gabriel.inocentes@gmail.com>
2025-02-04 14:14:06 -0500

    Base Code

--- Ch2Ex18V2.cpp
+++ Ch2Ex18V2.cpp
@@ -1,20 +1,20 @@
-// Ch2Ex18V2.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+/*
+    Program File Name: Ch2Ex3.cpp
+    Programmer: Gabriel Inocentes
+    Date: February 2, 2024
+    Requirements:
+    Write a program that applies a sales and county tax to a $95 purchase
+*/
 
 #include <iostream>
+using namespace std;
+
+const double purchasePrice = 95;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    float stateSalesTax = 0.04;
+    float countySalesTax = 0.02;
+
+    cout << "The full price of the purchase totals out to $" << purchasePrice * (stateSalesTax + countySalesTax + 1);
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
