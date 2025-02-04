cf8c771adc7f67fd7fba075c4d6c45d23d97158a
Gabriel Inocentes <gabriel.inocentes@gmail.com>
2025-02-04 14:17:58 -0500

    Includes Part 2 Requirements

--- Ch2Ex18V2.cpp
+++ Ch2Ex18V2.cpp
@@ -1,9 +1,9 @@
 /*
-    Program File Name: Ch2Ex3.cpp
+    Program File Name: Ch2Ex18.cpp
     Programmer: Gabriel Inocentes
     Date: February 2, 2024
     Requirements:
-    Write a program that applies a sales and county tax to a $95 purchase
+    Write a program that displays the sales tax on a purchase, which recieves the input of the user to determine the sales tax then applies it to the sale and display that total sales tax
 */
 
 #include <iostream>
@@ -13,8 +13,13 @@ const double purchasePrice = 95;
 
 int main()
 {
-    float stateSalesTax = 0.04;
-    float countySalesTax = 0.02;
+    float salesTax;
 
-    cout << "The full price of the purchase totals out to $" << purchasePrice * (stateSalesTax + countySalesTax + 1);
+    cout << "What is the local sales tax amount? ";
+    cin >> salesTax;
+    float salesTaxPercent = (salesTax / 100) + 1;
+    float salesTaxTotal = purchasePrice * salesTaxPercent;
+    float salesTaxAddition = salesTaxTotal - purchasePrice;
+    cout << "The full price of the purchase totals out to $" << salesTaxTotal << "\n";
+    cout << "The sales tax percentage is " << salesTax << "%, which on this purchase, totals to an additional cost of $" << salesTaxAddition;
 }
